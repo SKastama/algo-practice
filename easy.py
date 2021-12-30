c58ba4d80462dc640ac67324a9143949916923c5
Sarah Kastama <sarahkastama@gmail.com>
2021-12-30 15:33:12 -0800

    Algo practice 2

--- easy.py
+++ easy.py
@@ -45,4 +45,26 @@ def productSum(array, sums=0, mult=1):
         sums+= array[i]*mult
     return sums
 
-print(productSum([5, 2, [7, -1], 3, [6, [-13, 8], 4]]))
+# print(productSum([5, 2, [7, -1], 3, [6, [-13, 8], 4]]))
+
+def findThreeLargestNumbers(array):
+    largestNum= [None for i in range(3)]
+    i= 0
+    while i < len(array):
+        if largestNum[2] is None or array[i] > largestNum[2]:
+            shiftLargest(largestNum, 2, array[i])
+        elif largestNum[1] is None or array[i] > largestNum[1]:
+            shiftLargest(largestNum, 1, array[i])
+        elif largestNum[0] is None or array[i] > largestNum[0]:
+            shiftLargest(largestNum, 0, array[i])
+        i+= 1
+    return largestNum
+
+def shiftLargest(largeArray, idx, num):
+    for j in range(idx+1):
+        if j == idx:
+            largeArray[j]= num
+        else:
+            largeArray[j]= largeArray[j+1]
+
+print(findThreeLargestNumbers([1, 17, -7, -17, -27, 18, 141, 8, 7, 7, 541]))
